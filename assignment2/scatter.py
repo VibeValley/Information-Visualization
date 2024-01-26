922f68ee66874f687116c6a1ad6acd5f293963ef
Gustav Johansson <xgujoh1999@gmail.com>
2024-01-26 17:05:33 +0100

    Another commit
    Another initial commit

--- assignment2/scatter.py
+++ assignment2/scatter.py
@@ -1,1 +1,24 @@
 print('hej')
+
+class Point:
+  def __init__(self,x,y,type):
+    self.x = x
+    self.y= y
+    self.type = type
+
+    def __str__(self):
+        return f'{self.x}i{self.y:+}j'
+
+
+# load csv
+def load_csv(file):
+    data = []
+    with open(file, "r") as file:
+        for line in file:
+            values = line.strip().split(",")
+            data.append(Point(
+                float(values[0]),
+                float(values[1]),
+                values[2]
+            ))
+    return data
